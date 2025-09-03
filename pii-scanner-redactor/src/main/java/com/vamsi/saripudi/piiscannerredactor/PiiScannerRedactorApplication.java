bd29b41f88b6c56fcabddd231b28a5c3598a7d18
vamsisaripudi7514 <vamsi7514official@gmail.com>
2025-09-03 11:45:25 -0500

    init commit

---
+++ pii-scanner-redactor/src/main/java/com/vamsi/saripudi/piiscannerredactor/PiiScannerRedactorApplication.java
@@ -0,0 +1,13 @@
+package com.vamsi.saripudi.piiscannerredactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiiScannerRedactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiiScannerRedactorApplication.class, args);
+    }
+
+}
